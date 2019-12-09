3c2f5f6a1f9182cff5c08208c1ac9df962f2b41c
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-09 22:50:03 +0300

    SinoTibetan

---
+++ app/src/main/java/com/example/codecademy/SinoTibetan.java
@@ -0,0 +1,12 @@
+package com.example.codecademy;
+
+public class SinoTibetan extends Language {
+
+    public SinoTibetan(String languageName, int speakers){
+        super(languageName, speakers,"Asia","subject-object-verb");
+        if
+        (languageName.contains("Chinese")){
+            this.wordOrder = "subject-verb-object";
+        }
+    }
+}
