c5ec447f3a14721a1a5d60dd616c12f9be655a75
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-09 21:27:54 +0300

    Language

---
+++ app/src/main/java/com/example/codecademy/Language.java
@@ -0,0 +1,28 @@
+package com.example.codecademy;
+
+public class Language {
+    protected String name;
+    protected int numSpeakers;
+    protected String regionSpoken;
+    protected String wordOrder;
+
+    public Language(String langName, int speakers, String region, String wdOrder){
+        this.name = langName;
+        this.numSpeakers = speakers;
+        this.regionSpoken = region;
+        this.wordOrder = wdOrder;
+
+    }
+    public void getInfo(){
+        System.out.println(name+" is spoken by "+
+                numSpeakers+" people maily in "+
+                regionSpoken+". "+"The language follows the word order: "+
+                wordOrder+".");
+    }
+
+
+    public static void main(String[] args){
+        Language lang = new Language("English",2000000,"London","subject-verb-object");
+        lang.getInfo();
+    }
+}
